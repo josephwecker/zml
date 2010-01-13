c7768e17c51d78e3bdead910c7564330e9b01589
Joseph Wecker <joseph.wecker@gmail.com>
2010-01-12 21:02:54 -0800

    Basic translation to xml working.

--- src/zml.erl
+++ src/zml.erl
@@ -5,15 +5,32 @@
 
 compile(InFile) ->
   AST = zml_hand_parser:parse(zml_tokenizer:tokenize_file(InFile)),
-  AST.
-  %translate_ast_item(AST, []).
+  translate_ast_item(AST, []).
 
 translate_ast_item([], Acc) ->
   lists:reverse(Acc);
 translate_ast_item([String | [Next | _] = T], Acc)
     when is_list(String) and is_list(Next) ->
-  translate_ast_item(T, [list_to_binary(String) | [<<" ">> | Acc]]);
+  translate_ast_item(T, [" " | [String | Acc]]);
 translate_ast_item([String | T], Acc) when is_list(String) ->
-  translate_ast_item(T, [list_to_binary(String) | Acc]);
-translate_ast_item([_Tag | T], Acc) ->
-  translate_ast_item(T, [<<"(some tag)">> | Acc]).
+  translate_ast_item(T, [String | Acc]);
+translate_ast_item([{Name,Type,Attributes,[]} | T], Acc) ->
+  ToAppend = ["<", Name,
+    translate_attributes(Attributes), "/>"],
+  translate_ast_item(T, [ToAppend | Acc]);
+translate_ast_item([{Name,Type,Attributes,Children} | T], Acc) ->
+  ToAppend = [
+    "<", Name,
+    translate_attributes(Attributes), ">",
+    translate_ast_item(Children, []),
+    "</", Name, ">"],
+  translate_ast_item(T, [ToAppend | Acc]).
+
+translate_attributes([]) ->
+  "";
+translate_attributes(Atts) when is_tuple(Atts) ->
+  lists:foldl(fun out_attr/2, [], dict:to_list(Atts));
+translate_attributes(Atts) ->
+  lists:foldl(fun out_attr/2, [], Atts).
+out_attr({Name, Values}, Acc) ->
+  [" ", atom_to_list(Name), "=\"", string:join(Values, " "), "\"" | Acc].
