5d98f40c6167011c617d84aaadfb62373b7b12d8
Joseph Wecker <joseph@spreezio.com>
2010-01-21 16:22:04 -0800

    Tries real hard to find the handler.  Also has the handler dir passed in if it's off by itself for some reason.

--- src/zml.erl
+++ src/zml.erl
@@ -3,32 +3,49 @@
 -compile(export_all).
 
 compile(InFile) ->
+	{ok, CWD} = file:get_cwd(),
+	compile(InFile, CWD).
+
+compile(InFile, SpecialDir) ->
 	AST = zml_hand_parser:parse(zml_tokenizer:tokenize_file(InFile)),
 	SourceDir = filename:dirname(filename:absname(InFile)),
-	StagingDirName = integer_to_list(erlang:phash2(make_ref())),
+	StagingDirName = "tmp_" ++ integer_to_list(erlang:phash2(make_ref())),
 	{ok, CurrDir} = file:get_cwd(),
 	StagingDir = filename:join([CurrDir, StagingDirName]),
 	file:make_dir(StagingDir),
-	AST2 = run_specialized_handlers(AST, SourceDir, StagingDir),
+	AST2 = run_specialized_handlers(AST, SourceDir, StagingDir, SpecialDir),
 	Out = translate_ast_item(AST2, []),
 	io:format("~p", [Out]),
 	ok = file:write_file(filename:join([StagingDir, "output.html"]), Out),
 	StagingDir.
 
-run_specialized_handlers(AST, SourceDir, StagingDir) ->
-	run_spec_handler_inner(AST, SourceDir, StagingDir, AST).
+run_specialized_handlers(AST, SourceDir, StagingDir, SpecialDir) ->
+	run_spec_handler_inner(AST, SourceDir, StagingDir, SpecialDir, AST).
 
-run_spec_handler_inner([], _, _, NewAST) ->
+run_spec_handler_inner([], _, _, _, NewAST) ->
 	NewAST;
 run_spec_handler_inner([{{Name,ID}, special, Attr, Children} | T],
-		DSource, DStage, FullAST) ->
+		DSource, DStage, DSpec, FullAST) ->
 	HandlerName = list_to_atom("zml_special_" ++ string:to_lower(Name)),
-	{module, _} = code:ensure_loaded(HandlerName),
+	case code:ensure_loaded(HandlerName) of
+		{module, _} ->
+			great;
+		_ ->
+			HandlerSource = filename:join([DSpec, HandlerName]),
+			case code:load_abs(HandlerSource) of
+				{module, _} ->
+					whew;
+				{error, What} ->
+					erlang:error([
+								"'",What,"' When trying to load handler for ",
+								Name," special tag types."])
+			end
+	end,
 	NewAST = HandlerName:run_handler(ID, Attr, Children, FullAST, DSource,
 		DStage),
-	run_spec_handler_inner(T, DSource, DStage, NewAST);
-run_spec_handler_inner([H|T],DSource,DStage,FullAST) ->
-	run_spec_handler_inner(T, DSource, DStage, FullAST).
+	run_spec_handler_inner(T, DSource, DStage, DSpec, NewAST);
+run_spec_handler_inner([H|T],DSource,DStage,DSpec,FullAST) ->
+	run_spec_handler_inner(T, DSource, DStage, DSpec, FullAST).
 
 
 translate_ast_item([], Acc) ->
