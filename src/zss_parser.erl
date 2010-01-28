96b59d9b8f65001d32e9537cf7d88946a7d6ebc7
Joseph Wecker <joseph@spreezio.com>
2010-01-28 15:19:10 -0800

    Clumps like tokens together (except for dedent [there are never subsequent indents]).

--- src/zss_parser.erl
+++ src/zss_parser.erl
@@ -2,6 +2,27 @@
 
 -export([parse/1]).
 
-
+parse([]) ->
+  [];
 parse(Tokens) ->
-  Tokens.
+  Clumps = clumper(Tokens),
+  %parse(Tokens, {[],[]}, []).
+  Clumps.
+
+clumper([]) ->
+  [];
+clumper([{Type,_,_} = FirstTok | T]) ->
+  clumper(T, Type, [FirstTok], []).
+% Finished
+clumper([], _, [], Acc) ->
+  lists:reverse(Acc);
+clumper([], _, [{Type,_,_} | _] = CAcc, Acc) ->
+  lists:reverse([{Type, CAcc} | Acc]);
+% Same type
+clumper([{Type,_,_} = Tok | T], LastType, CAcc, Acc) when
+    (Type == LastType) and (Type /= dedent) ->
+  clumper(T, Type, [Tok | CAcc], Acc);
+% Different type
+clumper([{Type,_,_} = Tok | T], LastType, CAcc, Acc) ->
+  clumper(T, Type, [Tok], [{LastType, lists:keysort(3,CAcc)} | Acc]).
+
