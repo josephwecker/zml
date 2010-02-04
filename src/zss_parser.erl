7a833371767d8f3ed89b97a953f2f8554ee10758
Joseph Wecker <jwecker@JTVs-MacBook-Pro.local>
2010-02-03 19:14:48 -0800

    dnb - Working on parsing the inline codes etc.

--- src/zss_parser.erl
+++ src/zss_parser.erl
@@ -6,7 +6,8 @@ parse([]) ->
   [];
 parse(Tokens) ->
   Clumps = clumper(Tokens),
-  io:format("~p", [Clumps]),
+  %io:format("~p", [Clumps]),
+  erase(),   %% WARNING!!! Clobbers current process dictionary
   {[], {ChildRules, []}} = get_children([{}], Clumps),
   lists:sort(lists:map(fun format_rules/1, ChildRules)).
 
@@ -38,6 +39,18 @@ clumper([{Type,_,_} = Tok | T], LastType, CAcc, Acc) ->
   clumper(T, Type, [Tok], [format_clump(LastType, CAcc) | Acc]).
 
 
+format_clump(assignment, Asmts) ->
+  {assignment, lists:map(
+    fun({_,_,A}) ->
+      Str = string:sub_string(string:strip(A), 2, length(A) - 1),  % Remove brackets
+      {Left,Right} = lists:split(string:chr(Str,$=), Str),
+      {string:strip(string:sub_string(Left, 1, length(Left) - 1)), string:strip(Right)}
+    end, Asmts)};
+format_clump(code, Codes) ->
+  {code, lists:map(
+    fun({_,_,A}) ->
+      string:sub_string(string:strip(A),2,length(A) - 1)
+    end, Codes)};
 format_clump(indent, _) ->
   {indent, none};
 format_clump(end_of_file, _) ->
@@ -68,6 +81,28 @@ get_children(_Parents, [{end_of_file,_} | T], {RuleAcc, AttAcc}) ->
 get_children(_Parents, [{dedent,_} | T], {RuleAcc, AttAcc}) ->
   {T, {lists:sort(lists:flatten(RuleAcc)),
       lists:sort(lists:flatten(AttAcc))}};
+
+get_children(Parents, [{assignment, Asts} | T], {RuleAcc, AttAcc}) ->
+  lists:foreach(fun({Key,Val}) -> put("V" ++ Key, Val) end, Asts),
+  get_children(Parents, T, {RuleAcc, AttAcc});
+
+get_children(Parents, [{code,Codes} | [{indent,_} | T]], {RuleAcc, AttAcc}) ->
+  {T2, RawChildren} = pull_raw_children(T),
+  lists:foreach(fun(Key) -> put("M" ++ Key, RawChildren) end, Codes),
+  get_children(Parents, T2, {RuleAcc, AttAcc});
+
+get_children(Parents, [{code, Codes} | T], {RuleAcc, AttAcc}) ->
+  % TODO!! Turn this into a lfold to accumulate Rules / Attributes, and finish the function
+  lists:foreach(fun(Code) ->
+    {T2, {Rules, Atts}} =
+      case get("M" ++ Code) of
+        undefined ->
+          erlang:error("[" ++ Code ++ "] not defined before being used!"),
+          {[], {[], []}};
+        Vals ->
+          get_children(Parents, Vals, {RuleAcc, AttAcc})
+      end
+    end, Codes)
 
 get_children(Parents, [{attr,Atts} | T], {RuleAcc, AttAcc}) ->
   {T2, NewAtts} = get_attributes(Atts, T),
