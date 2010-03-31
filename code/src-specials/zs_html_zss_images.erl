1f6d26437027096bec645667a68cbddddd6f86fc
Joseph Wecker <joseph@spreezio.com>
2010-03-30 21:04:01 -0700

    Correctly detects magic file if appropriate, and otherwise combines styles correctly.  Doesn't do the alternative style attributes yet (like screen/print/etc.)

--- code/src-specials/zs_html_zss_images.erl
+++ code/src-specials/zs_html_zss_images.erl
@@ -10,8 +10,25 @@
 -export([process/5]).
 
 
-process(ID, Attr, Children, AST, Options) ->
-  SearchPaths = zml:get_search_paths(Options),
-
+process(_ID, Attr, _Children, AST, Options) ->
+  DeclaredZSS = get_declared_zss(Attr, Options),
+  io:format("~p", [DeclaredZSS]),
   AST.
+
+get_declared_zss(Attr, Options) ->
+  zml:get_attr_vals(style,Attr) ++
+  zml:get_attr_vals(styles,Attr) ++
+  case proplists:get_value(source_filename, Options, none) of
+    none -> [];
+    SFN ->
+      BaseName = filename:rootname(filename:basename(SFN)),
+      MagicZSSName = BaseName ++ ".zss",
+      SearchPaths = zml:get_search_paths(Options),
+      case file:path_open(SearchPaths, MagicZSSName, [read]) of
+        {ok, IOD, FullName} ->
+          file:close(IOD),
+          [FullName];
+        _ -> []
+      end
+  end.
 
