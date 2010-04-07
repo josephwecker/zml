3697dcd77282f5d56017d15ab3f7bb57c16a0f7d
Joseph Wecker <jwecker@99-204-11-157.pools.spcsdns.net>
2010-04-06 20:50:26 -0700

    Putting together the js plan

--- code/src-specials/zs_html_javascript.erl
+++ code/src-specials/zs_html_javascript.erl
@@ -1,6 +1,22 @@
+%% TODO:
+%%  - Do nothing when no scripts or magic scripts
+%%  - Inline page specific
+%%  - Auto detect and load jquery unless prohibited
+%%  - Option to turn off inlining page-specific
+%%  - Sequential Loader for external(s)
+%%  - Option to condense externals
+%%  - Option to optimize via clojure (results cached)
+%%  - Javascript libraries
+%%  - [future] callback to reload page when javascript changes
+%%  - [future] allow inline javascript specials?
+
 -module(zs_html_javascript).
 
 -export([process/5]).
 
-process(_ID, _Attr, _Children, AST, _Options) ->
+process(_ID, Attr, _Children, AST, Options) ->
+  MagicFile = zml:find_magic_file(".js", Options),
+  Externals = zml:get_attr_vals(script, Attr) ++
+    zml:get_attr_vals(scripts, Attr),
+
   AST.
