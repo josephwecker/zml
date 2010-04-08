5a572c4538f4695ee1a2352ec64d23aad3d8143a
Joseph Wecker <jwecker@70-6-253-31.pools.spcsdns.net>
2010-04-08 10:59:37 -0700

    Solidifying plan and moved some stuff around.  Top-down processing started.

--- code/src-specials/zs_html_javascript.erl
+++ code/src-specials/zs_html_javascript.erl
@@ -1,22 +1,51 @@
-%% TODO:
-%%  - Do nothing when no scripts or magic scripts
-%%  - Inline page specific
-%%  - Auto detect and load jquery unless prohibited
-%%  - Option to turn off inlining page-specific
-%%  - Sequential Loader for external(s)
-%%  - Option to condense externals
-%%  - Option to optimize via clojure (results cached)
-%%  - Javascript libraries
-%%  - [future] callback to reload page when javascript changes
-%%  - [future] allow inline javascript specials?
+%% Javascript portion of the "html" special handler
+
+%% Options (displayed with default)
+%%
+%%  The following can have one of 'inline', 'local', or 'external'
+%%   inline ->   Going to be in your html document
+%%   local ->    Actually pull in the files and deploy them (sometimes after
+%%               running optimizations etc. over them)
+%%   external -> Have a link to it and load it in normally
+%%
+%%  {js_magic_file,         inline}
+%%  {js_externals,          external}
+%%  {js_libs,               inline}
+%%  
+%%  {js_autoload_jquery,    true}  % simply adds a library entry before any other processing
+%%  {js_parallel_load,      true}
+%%  {js_load_at_bottom,     true}
+%%  
+%%  {js_script_dir,         "./scripts"}
+%%
+%%  (The last three require a script_dir and only work on "local")
+%%  {js_condense,           false}
+%%  {js_minify,             false}
+%%  {js_optimize,           false}
+%%
+%% Basic flow
+%% -------------
+%%  * gather inlines vs. local vs. externals
+%%  * local stuff processed and stored
+%%  * inlines, local output file(s), and externals combined according to
+%%    js_parallel_load and js_load_at_bottom
+%%
 
 -module(zs_html_javascript).
 
 -export([process/5]).
 
 process(_ID, Attr, _Children, AST, Options) ->
-  MagicFile = zml:find_magic_file(".js", Options),
-  Externals = zml:get_attr_vals(script, Attr) ++
+  MagicJS = zml:find_magic_file(".js", Options),
+  ExternalJS = zml:get_attr_vals(script, Attr) ++
     zml:get_attr_vals(scripts, Attr),
+  LibJS = zml:get_attr_vals(scriptlib, Attr) ++
+    zml:gett_attr_vals(scriptlibs, Attr) ++
+    autojquery(MagicJS),
+  
+  Inline = get_all_inline(MagicJS, ExternalJS, LibJS, Options),
+  Local = process_locals(MagicJS, ExternalJS, LibJS, Options),
+  External = get_externals(MagicJS, ExternalJS, LibJS, Options),
 
+  
   AST.
