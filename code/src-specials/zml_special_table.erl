fd76e3d95a8a6130433dcf09c1219273cf7c47b1
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-04-11 01:44:11 -0700

    implemented tr_* and td_* attributes in *table; still requires standalone | characters as table delimiters.

--- code/src-specials/zml_special_table.erl
+++ code/src-specials/zml_special_table.erl
@@ -9,34 +9,61 @@
 
 
 run_handler(ID, Attr, Children, AST, Options) ->
-  io:format("ID=~s Attr=~p Children=~p AST=~p Options=~p~n",
-            [ID, Attr, Children, AST, Options]),
-  Table = tr(Children, []),
-  zml:update_tag(AST, {"table", ID}, special, Attr, Table).
+  {[AttrTR, AttrTD], AttrTable} = extract_attrs(["tr_", "td_"], Attr),
+  Table = tr(Children, AttrTR, AttrTD, []),
+  zml:update_tag(AST, {"table", ID}, special, AttrTable, Table).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-tr([], Acc) -> lists:reverse(Acc);
+extract_attrs(Prefixes, Attrs) ->
+  extract_attrs(Prefixes, Attrs,
+    lists:map(fun(_) -> [] end, Prefixes), []).
 
-tr([newline | T], Acc) -> tr(T, Acc);
+extract_attrs(_Prefixes, [], Acc, Rest) ->
+  {Acc, lists:reverse(Rest)};
 
-tr(["|" | T], Acc) ->
-  {Tds, Rest} = td(T, [], []),
-  tr(Rest, [zml:new_tag(tr, [], Tds) | Acc]);
+extract_attrs(Prefixes, [H | T], Acc, Rest) ->
+  {NewAcc, Del} = extract_one_attr(Prefixes, H, Acc, [], false),
+  extract_attrs(Prefixes, T, NewAcc,
+    case Del of true -> Rest; _ -> [H | Rest] end).
 
-tr([H | _], _Acc) -> erlang:error(
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+extract_one_attr([Prefix | PT], {Key, Val}, [Acc | AT], Res, Found) ->
+  IsPrefix = lists:prefix(Prefix, Key),
+  extract_one_attr(PT, {Key, Val}, AT, [
+    case IsPrefix of
+      true -> [{string:substr(Key, string:len(Prefix) + 1), Val} | Acc];
+      _ -> Acc
+    end | Res ], Found or IsPrefix );
+
+extract_one_attr([], _, [], Res, Found) -> {lists:reverse(Res), Found}.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+tr([], _AttrTR, _AttrTD, Acc) -> lists:reverse(Acc);
+
+tr([newline | T], AttrTR, AttrTD, Acc) -> tr(T, AttrTR, AttrTD, Acc);
+
+tr(["|" | T], AttrTR, AttrTD, Acc) ->
+  {Tds, Rest} = td(T, AttrTD, [], []),
+  tr(Rest, AttrTR, AttrTD, [zml:new_tag(tr, AttrTR, Tds) | Acc]);
+
+tr([H | _], _AttrTR, _AttrTD, _Acc) -> erlang:error(
   "Table line must start with '|', got '" ++ H ++ "' instead.").
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-td(["|" | T], Acc, Tds) ->
-  td(T, [], [zml:new_tag(td, [], lists:reverse(Acc)) | Tds]);
+td(["|" | T], Attrs, Acc, Tds) ->
+  td(T, Attrs, [], [zml:new_tag(td, Attrs, lists:reverse(Acc)) | Tds]);
 
-td([newline | T], [], Tds) -> {lists:reverse(Tds), T};
+td([newline | T], _Attrs, [], Tds) -> {lists:reverse(Tds), T};
 
-td([newline | _], _, _) -> erlang:error("Table line must end with '|'.");
+td([newline | _], _, _, _) -> erlang:error("Table line must end with '|'.");
 
-td([], [], Tds) -> {lists:reverse(Tds), []};
+td([], _Attrs, [], Tds) -> {lists:reverse(Tds), []};
 
-td([], _, _) -> erlang:error("Table line must end with '|'.");
+td([], _, _, _) -> erlang:error("Table line must end with '|'.");
 
-td([H | T], Acc, Tds) -> td(T, [H | Acc], Tds).
+td([H | T], Attrs, Acc, Tds) -> td(T, Attrs, [H | Acc], Tds).
 
