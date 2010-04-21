b94046ff1ed38c99290b256f8f648b4f1c7c30cf
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-04-21 15:09:06 -0700

    removed run_zml.erl - replaced by bin/zml script

--- code/src/run_zml.erl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env escript
-%% -*- erlang -*-
-
-main(FNames) ->
-  case FNames of
-    [] -> io:format("~s~n", [zml:compile_stream(standard_io)]);
-    _  -> [io:format("~s~n", [zml:compile_file(F)]) || F <- FNames]
-  end.
-
