ffa94929177ac8d9c61df1e59a5322b6008a16c0
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-05-15 16:32:54 -0700

    added switching tokenizers on special characters and tags; no tags and/or special chars registered yet

--- code/src/zml_indent.erl
+++ code/src/zml_indent.erl
@@ -10,25 +10,42 @@
 
 tokenize_string(Str) ->
   Lines = string:tokens(Str, "\n"),
-  {Res, []} = tokenize(Lines, 0, recursive, []),
+  {Res, []} = tokenize(Lines, 0, recursive, no_tokenizer, []),
   Res.
 
-tokenize([], _Indent, _Rec, Acc) -> {lists:reverse(Acc), []};
+tokenize([], _Indent, _Rec, Tok, Acc) ->
+  {apply_tokenizer(Tok, Acc), []};
 
-tokenize([H | T] = Lines, Indent, Rec, Acc) ->
+tokenize([H | T] = Lines, Indent, Rec, Tok, Acc) ->
   {NewDent, Ln} = get_dent(H),
   if NewDent < Indent ->
-       {lists:reverse(Acc), Lines};
-     NewDent == Indent orelse Rec =:= non_recursive ->
-       tokenize(T, Indent, Rec, [Ln | Acc]);
-     NewDent > Indent ->
-       {L, R} = tokenize(T, NewDent, Rec, [Ln]),
-       tokenize(R, Indent, Rec, [L | Acc])
-  end.      
+       {apply_tokenizer(Tok, Acc), Lines};
+     Rec =:= non_recursive ->
+       tokenize(T, Indent, Rec, Tok, [Ln | Acc]);
+     true ->
+       {NewRec, NewTok, RestLn} = get_tokenizer(Rec, Ln),
+       case NewTok of
+         no_tokenizer ->
+           tokenize(T, Indent, Rec, Tok, [RestLn | Acc]);
+         _ ->
+           {L, R} = tokenize(T, NewDent, NewRec, NewTok, [RestLn]),
+           tokenize(R, Indent, Rec, Tok, [L | Acc])
+       end
+  end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 get_dent(Ln) -> get_dent(Ln, 0).
 get_dent([H|T], Dent) when ?IS_WHITESPACE(H) -> get_dent(T, Dent + 1);
 get_dent(Ln, Dent) -> {Dent, Ln}.
+
+
+apply_tokenizer(no_tokenizer, Acc) -> lists:reverse(Acc).
+
+
+get_tokenizer(non_recursive, Ln) ->
+  {non_recursive, no_tokenizer, Ln};
+
+get_tokenizer(recursive, Ln) ->
+  {recursive, no_tokenizer, Ln}.
 
