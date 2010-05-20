e7142764cee24258f8faf471959eb1c4b626de2a
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-05-20 00:18:56 -0700

    tag tokenizer added - so far it handles only |#...#| and |"..."| and \ escape; will add custom tokenizer for tag attributes and inline tags later

---
+++ code/src/zml_tag.erl
@@ -0,0 +1,50 @@
+
+-module(zml_tag).
+-compile(export_all).
+
+
+tokenize_tag(Lines) -> tokenize_tag(Lines, text, [], []).
+
+
+tokenize_tag([], text, AccL, AccR) ->
+  lists:reverse(add_tag(text, AccL, AccR));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+tokenize_tag([[$|, $# | Ln] | T], text, AccL, AccR) ->
+  tokenize_tag([Ln | T], comment, [], add_tag(text, AccL, AccR));
+
+tokenize_tag([[$#, $| | Ln] | T], comment, _AccL, AccR) ->
+  tokenize_tag([Ln | T], text, [], AccR);
+
+tokenize_tag([], comment, AccL, AccR) ->
+  tokenize_tag([], text, AccL ++ "#|", AccR);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+tokenize_tag([[$|, $" | Ln] | T], text, AccL, AccR) ->
+  tokenize_tag([Ln | T], quote, [], add_tag(text, AccL, AccR));
+  
+tokenize_tag([[$", $| | Ln] | T], quote, AccL, AccR) ->
+  tokenize_tag([Ln | T], text, [], add_tag(quote, AccL, AccR));
+
+tokenize_tag([], quote, AccL, AccR) ->
+  tokenize_tag([], text, AccL ++ "\"|", AccR);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+tokenize_tag([[$\\, Ch | Ln] | T], State, AccL, AccR) ->
+  tokenize_tag([Ln | T], State, [Ch | AccL], AccR);
+
+tokenize_tag([[Ch | Ln] | T], State, AccL, AccR) ->
+  tokenize_tag([Ln | T], State, [Ch | AccL], AccR);
+
+tokenize_tag([[] | T], State, AccL, AccR) ->
+  tokenize_tag(T, State, [$\n | AccL], AccR).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+add_tag( quote, AccL, AccR) -> [{text, lists:reverse(AccL)} | AccR];
+add_tag(_State, [],   AccR) -> AccR;
+add_tag( State, AccL, AccR) -> [{State, lists:reverse(AccL)} | AccR].
+
