eeaa111b036f352503ea4f44106f955a742e4a53
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-05-21 00:05:23 -0700

    zml_tag:tokenize_tag() now returns just a list of words, wrt comments and quoted lines. wee! next step is to parse attributes and inline tags.

--- code/src/zml_tag.erl
+++ code/src/zml_tag.erl
@@ -5,16 +5,13 @@
 
 tokenize_tag(Lines) -> tokenize_tag(Lines, text, [], []).
 
-tokenize_tag([], text, AccL, AccR) ->
-  lists:reverse(add_tag(text, AccL, AccR));
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 tokenize_tag([[$|, $| | _] | T], text, AccL, AccR) ->
-  tokenize_tag(T, text, [], add_tag(text, AccL, AccR));
+  tokenize_tag(T, text, [], add_text(text, AccL, AccR));
 
 tokenize_tag([[$|, $# | Ln] | T], text, AccL, AccR) ->
-  tokenize_tag([Ln | T], comment, [], add_tag(text, AccL, AccR));
+  tokenize_tag([Ln | T], comment, [], add_text(text, AccL, AccR));
 
 tokenize_tag([[$#, $| | Ln] | T], comment, _AccL, AccR) ->
   tokenize_tag([Ln | T], text, [], AccR);
@@ -25,10 +22,10 @@ tokenize_tag([], comment, AccL, AccR) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 tokenize_tag([[$|, Ch | Ln] | T], text, AccL, AccR) when ?IS_QUOTE(Ch) ->
-  tokenize_tag([Ln | T], {quote, Ch}, [], add_tag(text, AccL, AccR));
+  tokenize_tag([Ln | T], {quote, Ch}, [], add_text(text, AccL, AccR));
 
 tokenize_tag([[Ch, $| | Ln] | T], {quote, Q}, AccL, AccR) when Ch == Q ->
-  tokenize_tag([Ln | T], text, [], add_tag(quote, AccL, AccR));
+  tokenize_tag([Ln | T], text, [], add_text(quote, AccL, AccR));
 
 tokenize_tag([[$\\, Ch, $| | Ln] | T], {quote, Q}, AccL, AccR) when Ch == Q ->
   tokenize_tag([Ln | T], {quote, Q}, [$|, Ch | AccL], AccR);
@@ -36,26 +33,29 @@ tokenize_tag([[$\\, Ch, $| | Ln] | T], {quote, Q}, AccL, AccR) when Ch == Q ->
 tokenize_tag([], {quote, Q}, AccL, AccR) ->
   tokenize_tag([], text, AccL ++ [Q, $|], AccR);
 
+tokenize_tag([[] | T], {quote,_} = State, AccL, AccR) ->
+  tokenize_tag(T, State, [$\n | AccL], AccR);
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 tokenize_tag([[$\\, Ch | Ln] | T], text, AccL, AccR) ->
   tokenize_tag([Ln | T], text, [Ch | AccL], AccR);
 
+tokenize_tag([[Ch | Ln] | T], text, AccL, AccR) when ?IS_WHITESPACE(Ch) ->
+  tokenize_tag([Ln | T], text, [], add_text(text, AccL, AccR));
+
+tokenize_tag([[] | T], text, AccL, AccR) ->
+  tokenize_tag(T, text, [], add_text(text, AccL, AccR));
+
 tokenize_tag([[Ch | Ln] | T], State, AccL, AccR) ->
   tokenize_tag([Ln | T], State, [Ch | AccL], AccR);
 
-tokenize_tag([[] | T], State, AccL, AccR) ->
-  tokenize_tag(T, State, [$\n | AccL], AccR).
+tokenize_tag([], text, AccL, AccR) ->
+  lists:reverse(add_text(text, AccL, AccR)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-add_tag( quote, AccL, AccR) -> [{quote, lists:reverse(AccL)} | AccR];
-add_tag(_State, [],   AccR) -> AccR;
-add_tag( State, AccL, AccR) -> [{State, lists:reverse(AccL)} | AccR].
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-close_bracket($() -> $);
-close_bracket($[) -> $];
-close_bracket(${) -> $}.
+add_text( quote, AccL, AccR) -> [lists:reverse(AccL) | AccR];
+add_text(_State, [],   AccR) -> AccR;
+add_text(_State, AccL, AccR) -> [lists:reverse(AccL) | AccR].
 
