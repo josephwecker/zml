448b582d68127a8834f2df813ffc0776f14d9fde
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-05-24 12:44:33 -0700

    major rewrite of the tokenize_tag and parse_attr - still no inline tags, but must be much easier to add them now. fixed couple of bugs related to quoted attribute values, better tokenization of attributes, etc.

--- code/src/zml_tag.erl
+++ code/src/zml_tag.erl
@@ -3,120 +3,119 @@
 -compile(export_all).
 -include("zml_tokenizer.hrl").
 
-% tokenize_tag(Lines) -> tokenize_tag(Lines, text, [], []).
 
 tokenize_tag(Lines, Attr) ->
-  parse_attr(tokenize_tag(Lines, text, [], []), Attr).
+  {NewAttr, R} = parse_attr(Lines, Attr),
+  {NewAttr, tokenize_tag(R, [], [])}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-tokenize_tag([[$|, $| | _] | T], text, AccL, AccR) ->
-  tokenize_tag(T, text, [], add_text(text, AccL, AccR));
+tokenize_tag([[$|, $| | _] | T], AccL, AccR) ->
+  tokenize_tag(T, [], add_text(AccL, AccR));
 
-tokenize_tag([[$|, Ch | Ln] | T], text, AccL, AccR) when ?IS_QUOTE(Ch) ->
-  tokenize_tag([Ln | T], {quote, Ch}, [], add_text(text, AccL, AccR));
+tokenize_tag([[$|, Q | Ln] | T], AccL, AccR) when ?IS_QUOTE(Q) ->
+  {L,R} = parse_quote([Ln | T], Q, AccL),
+  tokenize_tag(R, L, AccR);
 
-tokenize_tag([[Q, $| | Ln] | T], {quote, Q}, AccL, AccR) ->
-  tokenize_tag([Ln | T], text, [], add_text(quote, AccL, AccR));
-
-tokenize_tag([], {quote, Q}, AccL, AccR) ->
-  tokenize_tag([], text, AccL ++ [Q, $|], AccR);
-
-tokenize_tag([[] | T], {quote,_} = State, AccL, AccR) ->
-  tokenize_tag(T, State, [32 | AccL], AccR);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-tokenize_tag([[$\\, Ch | Ln] | T], text, AccL, AccR)
+tokenize_tag([[$\\, Ch | Ln] | T], AccL, AccR)
     when ?IS_WHITESPACE(Ch) orelse Ch == $| ->
-  tokenize_tag([Ln | T], text, [Ch | AccL], AccR);
+  tokenize_tag([Ln | T], [Ch | AccL], AccR);
 
-tokenize_tag([[Ch | Ln] | T], text, AccL, AccR) when ?IS_WHITESPACE(Ch) ->
-  tokenize_tag([Ln | T], text, [], add_text(text, AccL, AccR));
+tokenize_tag([[Ch | Ln] | T], AccL, AccR) when ?IS_WHITESPACE(Ch) ->
+  tokenize_tag([Ln | T], [], add_text(AccL, AccR));
 
-tokenize_tag([[] | T], text, AccL, AccR) ->
-  tokenize_tag(T, text, [], [newline | add_text(text, AccL, AccR)]);
+tokenize_tag([[] | T], AccL, AccR) ->
+  tokenize_tag(T, [], [newline | add_text(AccL, AccR)]);
 
-tokenize_tag([Tag | T], text, AccL, AccR) when is_tuple(Tag) ->
-  tokenize_tag(T, text, [], [Tag | add_text(text, AccL, AccR)]);
+tokenize_tag([Tag | T], AccL, AccR) when is_tuple(Tag) ->
+  tokenize_tag(T, [], [Tag | add_text(AccL, AccR)]);
 
-tokenize_tag([[Ch | Ln] | T], State, AccL, AccR) ->
-  tokenize_tag([Ln | T], State, [Ch | AccL], AccR);
+tokenize_tag([[Ch | Ln] | T], AccL, AccR) ->
+  tokenize_tag([Ln | T], [Ch | AccL], AccR);
 
-tokenize_tag([], text, AccL, AccR) ->
-  lists:reverse(add_text(text, AccL, AccR)).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-add_text( quote, AccL, AccR) -> [lists:reverse(AccL) | AccR];
-add_text(_State, [],   AccR) -> AccR;
-add_text(_State, AccL, AccR) -> [lists:reverse(AccL) | AccR].
-
-close_bracket($() -> $);
-close_bracket($[) -> $];
-close_bracket(${) -> $}.
-
-add_attr({attr, Id, [32 | Val]}, Attr) ->
-  zml:append_attr(Attr, {Id, [lists:reverse(Val)]});
-
-add_attr({attr, Id, Val}, Attr) ->
-  zml:append_attr(Attr, {Id, [lists:reverse(Val)]}).
-
-append_rev([H|T], Acc) -> append_rev(T, [H|Acc]);
-append_rev([],    Acc) -> Acc.
+tokenize_tag([], AccL, AccR) -> lists:reverse(add_text(AccL, AccR)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-parse_attr([[Ch | W] | T] = Lines, Attr) when ?IS_BR_OPEN(Ch) ->
-  case parse_attr([W | T], id, {Ch, close_bracket(Ch), 0}, Attr) of
-    fail -> {Attr, Lines};
-    Res  -> Res
-  end;
+parse_attr([[Ch | W] | T], Attr) when ?IS_BR_OPEN(Ch) ->
+  parse_attr([W | T], none, [], [], {Ch, close_bracket(Ch), 0}, Attr);
 
-parse_attr([newline | T], Attr) -> parse_attr(T, Attr);
-parse_attr([[]      | T], Attr) -> parse_attr(T, Attr);
+parse_attr([[Ch | W] | T], Attr) when ?IS_WHITESPACE(Ch) ->
+  parse_attr([W | T], Attr);
+
+parse_attr([[] | T], Attr) -> parse_attr(T, Attr);
 
 parse_attr(Lines, Attr) -> {Attr, Lines}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-parse_attr([newline | T], id, Br, Attr) -> parse_attr(T, id, Br, Attr);
-parse_attr([[]      | T], id, Br, Attr) -> parse_attr(T, id, Br, Attr);
+parse_attr([[] | T], Id, AccL, AccR, Br, Attr) ->
+  parse_attr(T, Id, [], add_text(AccL, AccR), Br, Attr);
 
-parse_attr([W | T], id, Br, Attr) ->
+parse_attr([[Ch | W] | T], Id, AccL, AccR, Br, Attr) when ?IS_WHITESPACE(Ch) ->
+  parse_attr([W | T], Id, [], add_text(AccL, AccR), Br, Attr);
+
+parse_attr([[$|, $| | _] | T], Id, AccL, AccR, Br, Attr) ->
+  parse_attr([[] | T], Id, AccL, AccR, Br, Attr);
+
+parse_attr([[$|, Ch | W] | T], Id, AccL, AccR, Br, Attr) when ?IS_QUOTE(Ch) ->
+  {L,R} = parse_quote([W | T], Ch, AccL),
+  parse_attr(R, Id, L, AccR, Br, Attr);
+
+parse_attr([[Open | W] | T], Id, AccL, AccR, {Open, Close, L}, Attr) ->
+  parse_attr([W | T], Id, [Open | AccL], AccR, {Open, Close, L+1}, Attr);
+
+parse_attr([[Close | W] | T], Id, AccL, AccR, {_, Close, 0}, Attr) ->
+  {add_attr(Id, AccL, AccR, Attr), [W | T]};
+
+parse_attr([[Close | W] | T], Id, AccL, AccR, {Open, Close, L}, Attr) ->
+  parse_attr([W | T], Id, [Close | AccL], AccR, {Open, Close, L-1}, Attr);
+
+parse_attr([[$\\, Close | W] | T], Id, AccL, AccR, {_,Close,_} = Br, Attr) ->
+  parse_attr([W | T], Id, [Close | AccL], AccR, Br, Attr);
+
+parse_attr([[$\\, Ch | W] | T], Id,
+    AccL, AccR, Br, Attr) when ?IS_WHITESPACE(Ch) orelse Ch == $| ->
+  parse_attr([W | T], Id, [Ch | AccL], AccR, Br, Attr);
+
+parse_attr([W | T], Id, AccL, AccR, Br, Attr) ->
   case zml_indent:parse_id(W) of
-    {[_|_] = Id, ":"} -> parse_attr(T, {attr, Id, []}, Br, Attr);
-    _ -> fail
+    {[_|_] = NewId, [$:, Ch | R]} when ?IS_WHITESPACE(Ch) ->
+      parse_attr([R | T], NewId, [], [], Br, add_attr(Id, AccL, AccR, Attr));
+    {[_|_] = NewId, ":"} ->
+      parse_attr(T, NewId, [], [], Br, add_attr(Id, AccL, AccR, Attr));
+    {[_|_] = NewId, R} ->
+      parse_attr([R | T], Id, append_rev(NewId, AccL), AccR, Br, Attr);
+    {[], [Ch | R]} ->
+      parse_attr([R | T], Id, [Ch | AccL], AccR, Br, Attr)
   end;
 
-parse_attr([[] | T], {attr, Id, Val}, Br, Attr) ->
-  parse_attr(T, {attr, Id, [32 | Val]}, Br, Attr);
+parse_attr([], Id, AccL, AccR, _Br, Attr) ->
+  {add_attr(Id, AccL, AccR, Attr), []}.
 
-parse_attr([newline | T], {attr, Id, Val}, Br, Attr) ->
-  parse_attr(T, {attr, Id, [32 | Val]}, Br, Attr);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-parse_attr([[Open | W] | T], {attr, Id, Val}, {Open, Close, L}, Attr) ->
-  parse_attr([W | T], {attr, Id, [Open | Val]}, {Open, Close, L+1}, Attr);
+parse_quote([[Q, $| | W] | T], Q, Acc) -> {Acc, [W | T]};
+parse_quote([[Ch | W] | T], Q, Acc) -> parse_quote([W | T], Q, [Ch | Acc]);
+parse_quote([[] | T], Q, Acc) -> parse_quote(T, Q, [32 | Acc]);
+parse_quote([], _, Acc) -> {Acc, []}.
 
-parse_attr([[Close | W] | T], {attr,_,_} = State, {_, Close, 0}, Attr) ->
-  {add_attr(State, Attr), [W | T]};
 
-parse_attr([[Close | W] | T], {attr, Id, Val}, {Open, Close, L}, Attr) ->
-  parse_attr([W | T], {attr, Id, [Close | Val]}, {Open, Close, L-1}, Attr);
+add_text([],   AccR) -> AccR;
+add_text(AccL, AccR) -> [lists:reverse(AccL) | AccR].
 
-parse_attr([[$\\, Close | W] | T], {attr, Id, Val}, {_,Close,_} = Br, Attr) ->
-  parse_attr([W | T], {attr, Id, [Close | Val]}, Br, Attr);
 
-parse_attr([[Ch | W] = Word | T], {attr, Id, Val} = State, Br, Attr) ->
-  case zml_indent:parse_id(Word) of
-    {[_|_] = NewId, ":"} ->
-      parse_attr(T, {attr, NewId, []}, Br, add_attr(State, Attr));
-    {NewId, "\\:"} ->
-      parse_attr(T, {attr, Id, [$: | append_rev(NewId, Val)]}, Br, Attr);
-    {[_|_] = NewId, R} ->
-      parse_attr([R | T], {attr, Id, append_rev(NewId, Val)}, Br, Attr);
-    _ -> parse_attr([W | T], {attr, Id, [Ch | Val]}, Br, Attr)
-  end.
+add_attr(none, _AccL, _AccR, Attr) -> Attr;
 
-% parse_attr(_Lines, _State, _Br, _Attr) -> fail.
+add_attr(Id, AccL, AccR, Attr) ->
+ zml:append_attr(Attr, {Id, lists:reverse(add_text(AccL, AccR))}).
+
+
+append_rev([H|T], Acc) -> append_rev(T, [H|Acc]);
+append_rev([],    Acc) -> Acc.
+
+
+close_bracket($() -> $);
+close_bracket($[) -> $];
+close_bracket(${) -> $}.
 
