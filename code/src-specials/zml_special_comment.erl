fdcaeed48bc6b12e7f64543b8e169077af5f589e
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-06-23 16:39:16 -0700

    zml_special_comment:inline_tag/3 added (not tested yet)

--- code/src-specials/zml_special_comment.erl
+++ code/src-specials/zml_special_comment.erl
@@ -1,10 +1,12 @@
 
 -module(zml_special_comment).
 
--export([is_recursive/0, tokenize/3]).
+-export([is_recursive/0, tokenize/3, inline_tag/3]).
 
 
 is_recursive() -> {non_recursive, no_attrs, no_class_attrs}.
 
 tokenize("comment", _Attr, _Acc) -> [].
+
+inline_tag("comment", _Attr, _Body) -> [].
 
