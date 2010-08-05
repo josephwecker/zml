99980bf236918ea6198e5b9870e8b87e487e0255
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-08-04 17:34:31 -0700

    make pgsql_sample a regular module instead of hrl

---
+++ pgsql_sample.erl
@@ -0,0 +1,23 @@
+
+-module(pgsql_sample).
+-compile(export_all).
+
+merchants() ->
+  {ok,[{column,<<"idmerchant">>,int4,4,-1,0},
+       {column,<<"merchantname">>,varchar,-1,104,0},
+       {column,<<"logourl">>,varchar,-1,504,0},
+       {column,<<"lastupdated">>,date,4,-1,0},
+       {column,<<"isdeleted">>,bit,-1,1,0}],
+      [{<<"100059">>,<<"B.A. Mason">>,
+        <<"http://img.shopzilla.com/merchant/100059.gif">>,
+        <<"2009-01-10">>,<<"0">>},
+       {<<"100134">>,<<"LeGrand Treasures">>,
+        <<"http://img.shopzilla.com/merchant/100134.gif">>,
+        <<"2009-01-21">>,<<"0">>},
+       {<<"100293">>,<<"Austad\\'s Golf">>,
+        <<"http://img.shopzilla.com/merchant/100293.gif">>,
+        <<"2009-01-21">>,<<"0">>},
+       {<<"100436">>,<<"Need Plumbing Supplies">>,
+        <<"http://img.shopzilla.com/merchant/100436.gif">>,
+        <<"2009-01-21">>,<<"0">>}]}.
+
