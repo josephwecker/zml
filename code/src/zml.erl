1909a4321fc850ab08c2d174d66e05c270e4cc28
Sergei Matusevich <sergei.matusevich@gmail.com>
2010-08-16 00:08:20 -0700

    more refactorings and additions to the ets template cache interface: remaned all (internal) functions that translate zml into template to compile_*; added template_file and template_string functions that compile (and re-compile, if necessary) file or string to the ets cache; renames compile_files into compile_static_files, and changed bin/zml accordingly; define ZML_ZSS_LIBS default path in bin/zml; other minor reafctorings. [(maybe, almost..) ready to integrate templating with web framework and the database]

--- code/src/zml.erl
+++ code/src/zml.erl
@@ -2,8 +2,9 @@
 -module(zml).
 
 % Main function
--export([ compile_files/0, compile_files/1,
-          start/0, template_dir/2, render/3 ]).
+-export([ compile_static_files/0, compile_static_files/1,
+          template_dir/2, template_file/3, template_string/3,
+          start/0, render/3 ]).
 
 -include_lib("kernel/include/file.hrl").
 
@@ -12,17 +13,17 @@
         {proplists:get_value(Desc, Options),
          os:getenv(string:to_upper(atom_to_list(Desc)))}).
 
-compile_files() -> compile_files([]).
+compile_static_files() -> compile_static_files([]).
 
-compile_files([]) ->
-  Template = template_stream(standard_io, []),
+compile_static_files([]) ->
+  Template = compile_stream(standard_io, []),
   io:format("~s~n", [zml_render:render(Template, fake)]);
 
-compile_files(FLS) ->
+compile_static_files(FLS) ->
   lists:foreach(fun(FName) ->
     FNameOut = output_file_name(FName),
     io:format("~s --> ~s~n", [FName, FNameOut]),
-    Template = template_file(FName, []),
+    Template = compile_file(FName, []),
     ok = file:write_file(FNameOut, zml_render:render(Template, fake))
   end, FLS).
 
@@ -42,49 +43,54 @@ template_dir(Dir, Options) ->
     {ok, Files} ->
       lists:foreach(fun(FName) ->
         case string:right(FName, 5) of
-          [_|".zml"] -> add_template(Dir, FName, Options);
+          [_|".zml"] -> template_file(
+            list_to_atom(string:left(FName, string:len(FName) - 4)),
+            Dir ++ "/" ++ FName, Options);
           _ -> nothing
         end end, Files);
     Err -> Err
   end.
 
-add_template(Dir, FName, Options) ->
-  Path = Dir ++ "/" ++ FName,
+template_file(Id, Path, Options) ->
   {ok, FileInfo} = file:read_file_info(Path),
   NewTs = FileInfo#file_info.mtime,
-  Id = list_to_atom(string:left(FName, string:len(FName) - 4)),
   case ets:lookup(zml_templates, Id) of
     [{_Id, _Path, Ts, _Templ}] when Ts >= NewTs -> ok;
     _ -> ets:insert(zml_templates,
-      {Id, Path, NewTs, template_file(Path, Options)})
+      {Id, Path, NewTs, compile_file(Path, Options)})
   end.
 
-% @return ZML template or atom 'undefined'
-template(Id, Options) ->
+template_string(Id, Str, Options) ->
+  ets:insert(zml_templates, {Id, none, 0, compile_string(Str, Options)}).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% @return {ok, ZML template} or atom 'undefined'
+% recompile the file, if needed
+get_template(Id, Options) ->
   case ets:lookup(zml_templates, Id) of
-    [{Id, Path, Ts, Templ}] ->
+    [{_Id, none, 0,  Templ}] -> Templ;
+    [{_Id, Path, Ts, Templ}] ->
       {ok, FileInfo} = file:read_file_info(Path),
       NewTs = FileInfo#file_info.mtime,
-      case NewTs > Ts of
-        true  -> NewTempl = template_file(Path, Options),
+      {ok, case NewTs > Ts of
+        true  -> NewTempl = compile_file(Path, Options),
                  ets:insert(zml_templates, {Id, Path, NewTs, NewTempl}),
                  NewTempl;
         false -> Templ
-      end;
+      end};
     _ -> undefined
   end.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 render(Id, Data, Options) ->
-  case template(Id, Options) of
-    undefined -> undefined;
-    Templ -> zml_render:render(Templ, Data)
+  case get_template(Id, Options) of
+    {ok, Templ} -> zml_render:render(Templ, Data);
+    Err -> Err
   end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-template_file(InFile, Options) ->
+compile_file(InFile, Options) ->
   SourceFName = filename:absname(InFile),
   Options2 =
     case proplists:lookup(source_filename, Options) of
@@ -92,13 +98,13 @@ template_file(InFile, Options) ->
       _ -> Options
     end,
   {ok, Bin} = file:read_file(InFile),
-  template_string(binary_to_list(Bin), Options2).
+  compile_string(binary_to_list(Bin), Options2).
 
-template_stream(Stream, Options) ->
+compile_stream(Stream, Options) ->
   Str = io:get_chars(Stream, "", 1024000),
-  template_string(Str, Options).
+  compile_string(Str, Options).
 
-template_string(Str, Options) ->
+compile_string(Str, Options) ->
   Options2 = other_options(Options),
   AST = zml_indent:tokenize_string(Str), % TODO: pass options here
   AST2 = run_specialized_handlers(AST, Options2),
@@ -114,9 +120,7 @@ other_options(Options) ->
   case ?OPT_ENV(zml_closure_jar) of
     {undefined, V2} when V2 =/= false -> [{zml_closure_jar, V2}];
     _ -> []
-  end ++
-  Options.
-
+  end ++ Options.
 
 run_specialized_handlers(AST, Options) ->
   run_specialized_handlers_inner(AST, Options, AST).
@@ -136,7 +140,6 @@ run_specialized_handlers_inner(
 
 run_specialized_handlers_inner([_H|T], Options, FullAST) ->
   run_specialized_handlers_inner(T, Options, FullAST).
-
 
 translate_ast_item([], Acc) -> lists:reverse(Acc);
 translate_ast_item([newline | T], Acc) ->
